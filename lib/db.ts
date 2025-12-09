dabe79fc56b690043cdec6d11e4ef60fc3e2b43c
ankleshchaudhari <163991342+ankleshchaudhari@users.noreply.github.com>
2025-12-10 00:07:47 +0530

    DB connection fix

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 // Getting the MongoDB connection string from .env file
-const MONGODB_URI=process.env.MONGODB_URI!;
+const MONGODB_URI = process.env.MONGODB_URI!;
 
 // If no MongoDB URL found, stop the server & show error
-if(!MONGODB_URI){
+if (!MONGODB_URI) {
     throw new Error("Please define mongodb_uri in env variables");
 }
 
@@ -12,40 +12,35 @@ if(!MONGODB_URI){
 let cached = global.mongoose
 
 // If cache doesn't exist yet, create it
-if(!cached)
-{
-    cached=global.mongoose={conn:null , promise:null}
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null }
 }
 
-export async function connectionToDatabase()
-{
+export async function connectionToDatabase() {
     // If already connected, return the existing connection
-    if(cached.conn)
-    {
+    if (cached.conn) {
         return cached.conn
     }
 
     // If no connection promise exists, create one (Promose : Saves the connection process so you don't reconnect again)
-    if(!cached.promise)
-    {
+    if (!cached.promise) {
 
-        const opts={
+        const opts = {
             bufferCommands: true, // Mongoose stores commands(i.eany DB operations) before making actual connection
-                                 //Without buffering: our queries run too early and fail.
-            maxPoolSize:10, // Max number of DB connections
+            //Without buffering: our queries run too early and fail.
+            maxPoolSize: 10, // Max number of DB connections
         }
 
         // Create a new connection promise
-        mongoose.connect(MONGODB_URI,opts).then(()=>mongoose.connection)
+        cached.promise = mongoose.connect(MONGODB_URI, opts).then(() => mongoose.connection)
     }
 
-    try{
+    try {
         // Wait for the promise to finish → gives us the actual connection
-        cached.conn=await cached.promise
+        cached.conn = await cached.promise
     }
-    catch(error)
-    {
-        cached.promise=null;
+    catch (error) {
+        cached.promise = null;
         throw error;
     }
 
