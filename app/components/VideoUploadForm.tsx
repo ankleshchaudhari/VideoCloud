1eed666b308ceca30e8afa05877e9b41d1176d26
ankleshchaudhari <163991342+ankleshchaudhari@users.noreply.github.com>
2025-12-10 01:32:35 +0530

    implemented video

--- app/components/VideoUploadForm.tsx
+++ app/components/VideoUploadForm.tsx
@@ -55,7 +55,7 @@ export default function VideoUploadForm() {
 
     setLoading(true);
     try {
-      const res = await fetch("/api/video", {
+      const res = await fetch("/api/videos", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
